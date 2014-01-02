8b241de382af59113551f0de6bab46783ad837d2
Csaba Valyi <csaba_valyi@epam.com>
2014-01-02 14:18:43 +0100

    link to players added to header

--- app/js/controllers/headercontroller.js
+++ app/js/controllers/headercontroller.js
@@ -20,6 +20,10 @@ angular.module('fantasyApp.controllers.header', ['fantasyApp.services.login'])
         {
           "title": "Leagues",
           "link": "/leagues"
+        },
+        {
+          "title": "Players",
+          "link": "/players"
         }
       ];
       
